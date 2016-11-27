1464d5404f294e1c3690e739928cba266f6246da
秦建伟 <qinjianwei@qinjianweideMacBook-Pro.local>
2016-11-27 09:55:47 +0800

    Initial Commit

---
+++ WeFansiOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeFansiOS
+//
+//  Created by 秦建伟 on 2016/11/27.
+//  Copyright © 2016年 秦建伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeFansiOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeFansiOS
+//
+//  Created by 秦建伟 on 2016/11/27.
+//  Copyright © 2016年 秦建伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
